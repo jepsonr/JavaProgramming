5bb0ef189ef778fc644b25d1ff96b471e6f0abc9
jepsonr <jepsonr@gmail.com>
2016-01-16 16:29:07 -0700

    Update PerfectNumbers.java

--- PerfectNumbers.java
+++ PerfectNumbers.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-public class Perfect {
-
+public class Perfect {  
+	//returns a set of perfect numbers
 	public static void main(String[] args) {
 		//define variables and object
-		double limit;
+		long limit;
 		double exponent;
 		Scanner input = new Scanner(System.in);
 		
-		//enter a number greater than the desired set of perfect numbers
-		System.out.println("Enter upper limit for the set of " 
-				   + "perfect numbers up to 9 digits long");
-		limit = input.nextInt();
+		/*enter a number (less than 19 digits long) 
+		 * but greater than the largest desired perfect number*/
+		System.out.println("Enter upper limit for perfect "
+							+ "numbers (up to eighteen digits): ");
+		limit = input.nextLong();
 		
 		//calculation of Euclid-Euler theorem Mersenne primes
-		exponent = (Math.log(.5+Math.sqrt(2*limit+.25)) / Math.log(2));
+		exponent = (Math.log(.5+Math.sqrt(2*limit+.25)) 
+								/ Math.log(2));
 		/*cycle through the set of factors to find the set of primes
 		 associated with the user's chosen limit*/
 		for(int prime = 2; prime <= exponent; prime++){
@@ -39,7 +41,7 @@ public class Perfect {
 
 	public static void isPerfect(int p){
 		double perf = Math.pow(2, (p-1)) * (Math.pow(2, p) - 1);
-		int perfect = (int) Math.floor(perf);
+		long perfect = (long) Math.floor(perf);
 		System.out.println(perfect);
 	}//end function
 
