4fd5d409d1e410a3c5e9648c31ab86db3ced26d7
jepsonr <jepsonr@gmail.com>
2016-01-18 19:09:20 -0700

    Create Recording.java

---
+++ Recording.java
@@ -0,0 +1,27 @@
+
+public class Recording {
+	private String title;
+	private String artist;
+	private int playingTime;
+	
+	public void setTitle(String t){
+		title = t;
+	}
+	public String getTitle(){
+		return title;
+	}
+	
+	public void setArtist(String a){
+		artist = a;
+	}
+	public String getArtist(){
+		return artist;
+	}
+	
+	public void setPlayingTime(int p){
+		playingTime = p;
+	}
+	public int getPlayingTime(){
+		return playingTime;
+	}
+}
