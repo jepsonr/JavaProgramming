096c3336bcbff74590733fe59fd1b1ac8d4de24e
jepsonr <jepsonr@gmail.com>
2016-06-27 15:15:37 -0600

    Create Jovian.java
    child to Alien class and instantiated by the CreateAlien application

---
+++ Jovian.java
@@ -0,0 +1,12 @@
+
+public class Jovian extends Alien{
+
+	public Jovian(int classified) {
+		
+		//passes Jovian characteristics to the Alien Class
+		super("Jovian","purple", "methane");
+		
+		//sets the Alien variable passed by the CreateAlien app
+		classification = classified;
+	}
+}
