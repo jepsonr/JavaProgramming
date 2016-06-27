4eea06145ac5739ce3d149cda11de3c7346ab0bc
jepsonr <jepsonr@gmail.com>
2016-06-27 15:10:15 -0600

    Update CreateAliens.java

--- CreateAliens.java
+++ CreateAliens.java
@@ -1,11 +1,13 @@
 
 public class CreateAliens {
-
+	
+	//passes the classification to humanoid to each child of Alien
 	public static void main(String[] args) {
 		
 		Martian mars = new Martian(6);
 		Jovian jupiter = new Jovian(4);
 		
+		//calls the display fuction from each subclass to Alien class
 		mars.Description();
 		jupiter.Description();
 	}
