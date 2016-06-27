0fc817f998241914bb40aae9a23686fd3423592e
jepsonr <jepsonr@gmail.com>
2016-06-27 15:06:24 -0600

    Create CreateAliens.java

---
+++ CreateAliens.java
@@ -0,0 +1,12 @@
+
+public class CreateAliens {
+
+	public static void main(String[] args) {
+		
+		Martian mars = new Martian(6);
+		Jovian jupiter = new Jovian(4);
+		
+		mars.Description();
+		jupiter.Description();
+	}
+}
